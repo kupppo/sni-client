6f6eff6f0085dd6074bed0dac6ae8b085f38ea34
kupppo <kupppo@protonmail.com>
2026-09-14 23:07:20 +0100

    upgrade sentry

--- apps/web/app/global-error.tsx
+++ apps/web/app/global-error.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import * as Sentry from '@sentry/nextjs'
+import { captureException } from '@sentry/nextjs'
 import NextError from 'next/error'
 import { useEffect } from 'react'
 
@@ -10,11 +10,11 @@ export default function GlobalError({
   error: Error & { digest?: string }
 }) {
   useEffect(() => {
-    Sentry.captureException(error)
+    captureException(error)
   }, [error])
 
   return (
-    <html>
+    <html lang="en">
       <body>
         {/* `NextError` is the default Next.js error page component. Its type
         definition requires a `statusCode` prop. However, since the App Router
--- apps/web/instrumentation-client.ts
+++ apps/web/instrumentation-client.ts
@@ -1,6 +1,8 @@
-import { init } from '@sentry/nextjs'
+import { captureRouterTransitionStart, init } from '@sentry/nextjs'
 
 init({
   dsn: 'https://a966e1ae42e2794871255e7fe2851a02@o4507844184834048.ingest.us.sentry.io/4508210706841600',
   tracesSampleRate: 0.1,
 })
+
+export const onRouterTransitionStart = captureRouterTransitionStart
